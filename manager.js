65fd0d5a5725d19468c41949d3b8310344333e75
graphite <admin@ohhere.xyz>
2017-07-28 15:03:46 +0800

    ok by copy_only

--- manager.js
+++ manager.js
@@ -1,18 +1,13 @@
-var data = [
-  ["", "Ford", "Volvo", "Toyota", "Honda"],
-  ["2016", 10, 11, 12, 13],
-  ["2017", 20, 11, 14, 13],
-  ["2018", 30, 15, 12, 13]
-];
+var column_names = ['user_id', 'volunteer_id', 'username', 'student_id', 'legal_name', 'phone'];
+column_names = column_names.concat(['email', 'gender', 'age', 'volunteer_time', 'note'])
 
 var container = document.getElementById('example');
 var hot = new Handsontable(container, {
-  rowHeaders: true,
-  colHeaders: true,
+  colHeaders: column_names,
   columns: [
     {data: 'user_id'},
     {data: 'volunteer_id'},
-    {data: 'username'},
+    {data: 'username'}, 
     {data: 'student_id'},
     {data: 'legal_name'},
     {data: 'phone'},
@@ -25,19 +20,14 @@ var hot = new Handsontable(container, {
 });
 
 function load_data() {
-  $.ajax(
-    {
-      url: "https://own.ohhere.xyz/volunteers?length=1",
-      type: "GET",
-      dataType: "json",
-      async: true,
-      success: function (res) {
-        console.log(res)
-        json = JSON.parse(res)
-        hot.loadData(json);
-      }
-    }
-  )
+  $.getJSON("https://own.ohhere.xyz/volunteers?length=5", function(res) {
+      console.log(res);
+      $.each(res, function(vol_index, one_vol) {
+        json_one = JSON.parse(one_vol)
+        console.log(json_one);
+        hot.alter('insert_row', hot.countRows(), 1, json_one);
+      })
+    });
 }
 
 load_data();
