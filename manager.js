33f5cf279e3d3b5480e97bdceff4579e4449f579
graphite <admin@ohhere.xyz>
2017-07-29 17:18:52 +0800

    show data by table_lines render-able

--- manager.js
+++ manager.js
@@ -1,6 +1,6 @@
 var column_names = ['user_id', 'volunteer_id', 'username', 'student_id', 'legal_name', 'phone'];
 column_names = column_names.concat(['email', 'gender', 'age', 'volunteer_time', 'note'])
-
+var table_lines = Array();
 var container = document.getElementById('example');
 var hot = new Handsontable(container, {
   colHeaders: column_names,
@@ -16,17 +16,19 @@ var hot = new Handsontable(container, {
     {data: 'age'},
     {data: 'volunteer_time'},
     {data: 'note'},
-  ]
+  ],
+  data: table_lines,
 });
 
+
 function load_data() {
-  $.getJSON("https://own.ohhere.xyz/volunteers?length=5", function(res) {
-      console.log(res);
-      $.each(res, function(vol_index, one_vol) {
-        json_one = JSON.parse(one_vol)
-        console.log(json_one);
-        hot.alter('insert_row', hot.countRows(), 1, json_one);
-      })
+  $.getJSON("https://own.ohhere.xyz/volunteers?length=5", function(raw_lines) {
+      console.log(raw_lines);
+      $.each(raw_lines, function(line_index, raw_line) {
+        table_lines[line_index] = JSON.parse(raw_line)
+        // console.log(line);
+      });
+      hot.render();
     });
 }
 
