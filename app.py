e94e1b8dcf2002ca56e38cee18d8c02ed77fb415
graphite <admin@ohhere.xyz>
2017-08-07 20:36:02 +0800

    add create_main_blueprint

--- app.py
+++ app.py
@@ -2,20 +2,24 @@
 # -*- coding: UTF-8 -*-
 
 from flask import Flask
-from api_handle import get_api
+from api_handle import create_api
 from auth_handle import bcrypt
 from config import AppConfig
-from main import main_blueprint
+from main.views import create_main_blueprint
 from mess import fun_logger
 from tables import db
+import logging
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(AppConfig)
-    api = get_api()
+    api = create_api()
     api.init_app(app)
     bcrypt.init_app(app)
     db.init_app(app)
     AppConfig.init_app(app)
+    main_blueprint = create_main_blueprint()
     app.register_blueprint(main_blueprint)
+    logging.info('%r', app.view_functions)
+    logging.info('%r', app.url_map)
     return app
--- main/__init__.py
+++ main/__init__.py
@@ -1,6 +1,2 @@
 #!/usr/env/python3
 # -*- coding: UTF-8 -*-
-
-from flask import Blueprint
-main_blueprint = Blueprint('main', __name__, template_folder='templates')
-from . import views
