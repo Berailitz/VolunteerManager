b3b558eaa8ee44aa074b5514cbce1a205690422b
graphite <admin@ohhere.xyz>
2017-08-07 18:52:53 +0800

    register blueprint in app.py

--- main/__init__.py
+++ main/__init__.py
@@ -4,6 +4,3 @@
 from flask import Blueprint
 main_blueprint = Blueprint('main', __name__, template_folder='templates')
 from . import views
-
-def create_main_blueprint(app):
-    app.register_blueprint(main_blueprint)
