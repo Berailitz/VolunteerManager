122a240cd37950d8053264b16bff2887d337d895
graphite <admin@ohhere.xyz>
2017-08-07 18:25:35 +0800

    remove diff

--- run.py
+++ run.py
@@ -2,11 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 from mess import set_logger
-<<<<<<< HEAD
 from . import create_app
-=======
-from VolunteerManager import create_app
->>>>>>> 3c302a805d82775cd5978470567076756fa42cfe
 # import ptvsd
 
 # ptvsd.enable_attach("passw0", address=('0.0.0.0', 989))
