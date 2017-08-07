7284e74f8f17f10e9b951c5cf45f4cb8aa55b678
graphite <admin@ohhere.xyz>
2017-08-07 18:52:53 +0800

    register blueprint in app.py

--- app.py
+++ app.py
@@ -5,6 +5,7 @@ from flask import Flask
 from api_handle import api, init_api, job_api, record_api, relationship_api, token_api, volunteer_api
 from auth_handle import bcrypt
 from config import AppConfig
+from main import main_blueprint
 from tables import db
 
 def create_app():
@@ -15,4 +16,5 @@ def create_app():
     bcrypt.init_app(app)
     db.init_app(app)
     AppConfig.init_app(app)
+    app.register_blueprint(main_blueprint)
     return app
