86a4d97a6a129f2ee4bbce208fe6ac18c3c756a3
graphite <admin@ohhere.xyz>
2017-08-08 23:00:27 +0800

    add SECRET_KEY

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ class AppConfig:
     SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://xh:xh@localhost/xh?charset=utf8'
     SQLALCHEMY_TRACK_MODIFICATIONS = True
     TEMPLATES_AUTO_RELOAD = True
-    
+    SECRET_KEY = '67gyubinjmokl,pl'
+
     @staticmethod
     def init_app(app):
         app.config.update(RESTFUL_JSON=dict(ensure_ascii=False))
