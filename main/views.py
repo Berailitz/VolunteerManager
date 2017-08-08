f4e595ef1dd496a2414cad3d8211179ff7b4487a
graphite <admin@ohhere.xyz>
2017-08-08 18:42:41 +0800

    add guest_only, admin_only

--- main/views.py
+++ main/views.py
@@ -2,6 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 from flask import Blueprint, render_template
+from ..auth_handle import admin_only, guest_only
 
 def create_main_blueprint():
     main_blueprint = Blueprint('main', __name__, template_folder='templates')
@@ -11,14 +12,18 @@ def create_main_blueprint():
     main_blueprint.add_url_rule('/volunteer', 'volunteer', show_volunteer_page)
     return main_blueprint
 
+@guest_only
 def show_index_page():
     return render_template('index.html')
 
+@admin_only
 def show_project_page():
     return render_template('project.html')
 
+@admin_only
 def show_record_page():
     return render_template('record.html')
 
+@admin_only
 def show_volunteer_page():
     return render_template('volunteer.html')
