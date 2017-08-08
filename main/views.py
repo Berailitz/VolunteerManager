069db2a6dc25774f0ff64db1cedd7341d5a68719
graphite <admin@ohhere.xyz>
2017-08-08 20:13:12 +0800

    add @fun_logger('login')

--- main/views.py
+++ main/views.py
@@ -3,6 +3,7 @@
 
 from flask import Blueprint, render_template
 from ..auth_handle import admin_only, guest_only
+from ..mess import fun_logger
 
 def create_main_blueprint():
     main_blueprint = Blueprint('main', __name__, template_folder='templates')
@@ -12,11 +13,13 @@ def create_main_blueprint():
     main_blueprint.add_url_rule('/volunteer', 'volunteer', show_volunteer_page)
     return main_blueprint
 
+@fun_logger('login')
 @guest_only()
 def show_index_page():
     return render_template('index.html')
 
 @admin_only()
+@fun_logger('login')
 def show_project_page():
     return render_template('project.html')
 
