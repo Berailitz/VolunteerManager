e00e02b877eb8a0d15ad4f604225ebe73a94864f
graphite <admin@ohhere.xyz>
2017-08-08 18:54:05 +0800

    adapt to from .. import auth_handle

--- main/views.py
+++ main/views.py
@@ -2,7 +2,7 @@
 # -*- coding: UTF-8 -*-
 
 from flask import Blueprint, render_template
-from ..auth_handle import admin_only, guest_only
+from .. import auth_handle
 
 def create_main_blueprint():
     main_blueprint = Blueprint('main', __name__, template_folder='templates')
@@ -12,18 +12,18 @@ def create_main_blueprint():
     main_blueprint.add_url_rule('/volunteer', 'volunteer', show_volunteer_page)
     return main_blueprint
 
-@guest_only
+@auth_handle.guest_only
 def show_index_page():
     return render_template('index.html')
 
-@admin_only
+@auth_handle.admin_only
 def show_project_page():
     return render_template('project.html')
 
-@admin_only
+@auth_handle.admin_only
 def show_record_page():
     return render_template('record.html')
 
-@admin_only
+@auth_handle.admin_only
 def show_volunteer_page():
     return render_template('volunteer.html')
