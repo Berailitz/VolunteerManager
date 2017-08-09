1a202696e50b82db6d343134c4e6b21c34d04dc0
graphite <admin@ohhere.xyz>
2017-08-09 22:15:53 +0800

    re-arrange files

--- run.py
+++ run.py
@@ -1,19 +1,14 @@
 #!/usr/env/python3
 # -*- coding: UTF-8 -*-
 
-from .mess import set_logger
-from .app import create_app
 import logging
-# import ptvsd
-
-# ptvsd.enable_attach("passw0", address=('0.0.0.0', 989))
-# ptvsd.wait_for_attach()
+from VolunteerManager import app, mess
 
 def main():
-    set_logger('log.txt')
+    mess.set_logger('log.txt')
     logging.info('start...')
-    app = create_app()
-    app.run(debug=True)
+    flask_app = app.create_app()
+    flask_app.run(debug=True)
 
 if __name__ == '__main__':
     main()
