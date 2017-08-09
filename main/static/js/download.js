cb9a5cc5fbf4ae385787159e4388f73a5198c7c3
graphite <admin@ohhere.xyz>
2017-08-09 18:23:16 +0800

    change url and title

--- main/static/js/download.js
+++ main/static/js/download.js
@@ -2,7 +2,7 @@
 
 function download() {
     let type_dict = {
-        '活动记录补全版': '0',
+        '活动记录补全版': 'all_in_one',
         '原始活动原始表格': 'records',
         '志愿者信息原始表格': 'volunteers',
         '志愿项目原始表格': 'jobs'
