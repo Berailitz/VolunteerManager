4d9978505446d7d6bb82ace44a2fc6574bb8a59b
graphite <admin@ohhere.xyz>
2017-08-09 13:03:34 +0800

    add showToast('登录中', 700);

--- main/static/js/index.js
+++ main/static/js/index.js
@@ -18,6 +18,7 @@ function set_token(token) {
 }
 
 function login() {
+  showToast('登录中', 700);
   $.getJSON('https://own.ohhere.xyz/api/tokens', {
     'username': document.getElementById('username-box').value,
     'password': document.getElementById('password-box').value
