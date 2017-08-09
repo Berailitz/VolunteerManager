360826cbd6e2071d13461b1af3e6a1121c804f66
graphite <admin@ohhere.xyz>
2017-08-10 01:38:03 +0800

    add request.args

--- VolunteerManager/auth_handle.py
+++ VolunteerManager/auth_handle.py
@@ -114,6 +114,7 @@ def load_token(update_token=True, error_status_code=1):
         def wrapper(*args, **kw):
             parser = reqparse.RequestParser()
             parser.add_argument('token', type=str)
+            logging.info('%r', request.args)
             logging.info(parser.parse_args())
             admin = get_arg(parser.parse_args()['token'], None, lambda token: check_token(token))
             if admin:
