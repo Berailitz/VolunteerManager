3aa364fc08081f9dd7997f58813f620c7281c3af
graphite <admin@ohhere.xyz>
2017-08-09 22:17:28 +0800

    add logger.setLevel

--- run.py
+++ run.py
@@ -6,6 +6,8 @@ from VolunteerManager import app, mess
 
 def main():
     mess.set_logger('log.txt')
+    logger = logging.getLogger()
+    logger.setLevel(logging.INFO)
     logging.info('start...')
     flask_app = app.create_app()
     flask_app.run(debug=True)
