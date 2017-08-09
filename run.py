701788fa7cadd94154f498ca140b6782f52e6549
graphite <admin@ohhere.xyz>
2017-08-09 23:02:21 +0800

    expose application

--- run.py
+++ run.py
@@ -4,13 +4,14 @@
 import logging
 from VolunteerManager import app, mess
 
+application = app.create_app()
+
 def main():
     mess.set_logger('log.txt')
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     logging.info('start...')
-    flask_app = app.create_app()
-    flask_app.run(debug=True)
+    application.run(debug=True)
 
 if __name__ == '__main__':
     main()
