333fefdadd6479fd0ed2d7817cc122e4106df087
graphite <admin@ohhere.xyz>
2017-09-26 17:10:42 +0800

    add default credentials

---
+++ VolunteerManager/credentials.default.py
@@ -0,0 +1,9 @@
+"""credentials of this app"""
+#!/usr/env/python3
+# -*- coding: UTF-8 -*-
+
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://username:password@localhost/database?charset=utf8'
+SECRET_KEY = 'xxx'
+SYNC_UAERNAME = 'username'
+SYNC_ENCRYPTED_PASSWORD = "password"
+UNIVERSAL_DEBUG_TOKEN = 'token' # IMPORTANT NOTE: FOR DEBUG ONLY
