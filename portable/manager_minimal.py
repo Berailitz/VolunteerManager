55633cbd451ac236289094a2bbb38cb906e75970
graphite <admin@ohhere.xyz>
2017-12-03 15:55:52 +0800

    add manager_minimal.py, manager_portable.py and initialize.bat

---
+++ portable/manager_minimal.py
@@ -0,0 +1,165 @@
+"""Portable manager class for `bv2008.cn`"""
+#!/usr/bin/python3
+# -*- coding: UTF-8 -*-
+import logging
+import random
+import sys
+import time
+try:
+    import requests
+except ImportError as requests_import_error:
+    print(requests_import_error.msg)
+    requests_import_error.msg = "Please make sure to install package `requests` first."
+    requests_import_error.msg += " Run `pip install requests` or"
+    requests_import_error.msg += " refer to `http://docs.python-requests.org/zh_CN/latest/user/install.html#install`."
+    raise requests_import_error
+
+def set_logger(log_file_path):
+    """set logger"""
+    logging.basicConfig(
+        level=logging.INFO,
+        format='[%(levelname)s] %(asctime)s %(filename)s:%(lineno)d %(message)s',
+        datefmt='%Y %b %d %H:%M:%S',
+        filename=log_file_path,
+        filemode='a'
+        )
+    console = logging.StreamHandler()
+    console.setLevel(logging.INFO)
+    formatter = logging.Formatter('[%(levelname)s] %(asctime)s %(filename)s:%(lineno)d %(message)s')
+    console.setFormatter(formatter)
+    logging.getLogger('').addHandler(console)
+    logging.info("Start ...")
+
+class Manager(object):
+    """Manage volunteers on bv2008, whose `volunteer_list` is a list of objects"""
+    def __init__(self):
+        self.my_session = requests.Session()
+        self.volunteer_list = list()
+        self.json_path = 'volunteer_list.json'
+
+    @staticmethod
+    def create_headers(referer):
+        """create http headers with custom `referer`"""
+        headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win) AppleWebKit/537.36 (KHTML, like Gecko) Safari/537.36',
+            'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8',
+            'Accept-Language': 'en-US,en;q=0.8,zh-CN;q=0.6,zh;q=0.4',
+            'Connection': 'keep-alive',
+            'Accept-Encoding': 'gzip, deflate',
+            'Referer': referer,
+        }
+        return headers
+
+    def post(self, url, data, timeout=10, max_retries=10, referer='http://www.bv2008.cn', **kw):
+        """customized post"""
+        for attempt_times in range(max_retries):
+            try:
+                post_response = self.my_session.post(url, headers=self.create_headers(referer), data=data, timeout=timeout, **kw)
+                break
+            except requests.Timeout as identifier:
+                attempt_times += 1
+                logging.warning(f'Syncer failed to POST `{str(data)}` to `{url}`: {str(identifier)}')
+        post_response.encoding = "utf-8-sig"
+        return post_response
+
+    def get(self, url, timeout=10, max_retries=10, referer='http://www.bv2008.cn', **kw):
+        """customized get"""
+        for attempt_times in range(max_retries):
+            try:
+                get_response = self.my_session.get(url, headers=self.create_headers(referer), timeout=timeout, **kw)
+                break
+            except requests.Timeout as identifier:
+                attempt_times += 1
+                logging.warning(f'Syncer failed to GET `{url}`: {str(identifier)}')
+        get_response.encoding = "utf-8-sig"
+        return get_response
+
+    def login(self, username, encrypted_password):
+        """login with encrypted password"""
+        if not username or not encrypted_password:
+            logging.error('[Failed]No username or password specified.')
+            raise KeyError('No username or password specified.')
+        login_url = 'http://www.bv2008.cn/app/user/login.php?m=login'
+        login_payload = {"uname": username, "upass": encrypted_password}
+        login_response = self.post(login_url, login_payload)
+        login_json = login_response.json()
+        if login_json['code'] == 0:
+            logging.info(f"[Succeeded]Login as {username}")
+            return True
+        else:
+            logging.error(f"[Failed]Faied to login: {login_json['msg']}")
+            return False
+
+    def invite(self, project_id, job_id, volunteer_id_list):
+        """invite a list of volunteers to a project"""
+        invite_url = f'http://www.bv2008.cn/app/opp/opp.my.php?m=invite&item=recruit&opp_id={project_id}&job_id={job_id}'
+        invite_payload = {'stype':'local', 'uid[]': volunteer_id_list}
+        invite_response = self.post(invite_url, invite_payload)
+        response_json = invite_response.json()
+        logging.info(f"[Unknown]Invite info: {response_json['msg']}")
+        return invite_response.json()
+
+    def import_record_text(self, project_id, job_id, id_type, record_text):
+        """#DEBUG#: add records in text, may fail due to frequency limits"""
+        # id_type: 1 for user id, 3 for volunteer id, 4 for legal id
+        record_url = f'http://www.bv2008.cn/app/opp/opp.my.php?manage_type=0&m=import_hour&item=hour&opp_id={project_id}'
+        record_payload = {'content': record_text, 'vol_type': id_type, 'opp_id': project_id, 'job_id': job_id}
+        record_response = self.post(record_url, record_payload)
+        response_json = record_response.json()
+        logging.info("import_record_text -> response_json: " + str(response_json)) # for debug
+        if response_json['code'] == 0:
+            logging.info(f"[Unknown]Record: {response_json['msg']}")
+        if 'data' in response_json.keys():
+            for recorded_item in response_json['data']:
+                success_log = "[Successful]: #{vol_id} -> {hour_num} hours @ job #{job_id}, opp #{opp_id}: {msg}".format(**recorded_item)
+                logging.info(success_log)
+        if 'failed' in response_json.keys():
+            for failed_item in response_json['failed']:
+                error_log = "[Failed]: #{vol_id} -> {hour_num} hours @ job #{job_id}, opp #{opp_id}: {msg}".format(**failed_item)
+                logging.error(error_log)
+        return response_json
+
+    def save_record_item(self, project_id, job_id, user_id, working_time, record_note):
+        """save one record"""
+        record_url = f'http://www.bv2008.cn/app/opp/opp.my.php?manage_type=0&m=save_hour&item=hour&opp_id={project_id}&job_id={job_id}'
+        record_payload = {'hour_num': str(working_time), 'memo': str(record_note), 'uid[]': str(user_id)}
+        record_response = self.post(record_url, record_payload)
+        response_json = record_response.json()
+        logging.info("save_record_item -> response_json: " + str(response_json)) # for debug
+        if response_json['code'] == 0:
+            logging.info(f"[Successful]Record: {response_json['msg']}")
+        else:
+            logging.info(f"[Failed]Record: #{response_json['id']} ERROR{response_json['code']} {response_json['msg']}")
+        return response_json
+
+def main():
+    """main function"""
+    ###################################################################
+    ########################只需修改下面几行############################
+    my_volunteer_uid_list = [27140874, 27264014, 27299558] #志愿者的UID
+    my_project_id = 1164518 #项目ID
+    my_job_id = 1495453 # 岗位ID
+    my_hour = 6 # 时长
+    my_note = "这里写备注" # 备注
+    ########################只需修改上面几行############################
+    ###################################################################
+    set_logger("volunteer_log.txt")
+    if sys.version_info < (3, 6):
+        raise RuntimeError('Please install Python 3.6.2 or above. See `https://www.python.org/getit/`')
+    time_to_sleep = 3
+    encrypted_password = r"VsNl91lWRJpjkVCTVL4j/pa2w1Ij+U0JqNHIoWCYiGZy5+246J+1UDIs+aplYoH4DiHVfk+jkzGDijqc6ZLsb8mhrj"
+    encrypted_password += r"WOO/CdZ7tD5rn5+Wd6yFgXnRoiaZGAiaAxiPONZuVce11IyOyISchMapiV8b4G8GyREbEg+pcRuhz5Y3Q="
+    my_manager = Manager()
+    login_status = my_manager.login("scsfire", encrypted_password)
+    if not login_status:
+        exit()
+    my_manager.invite(my_project_id, my_job_id, my_volunteer_uid_list)
+    for single_volunteer_uid in my_volunteer_uid_list:
+        my_manager.save_record_item(my_project_id, my_job_id, single_volunteer_uid, my_hour, my_note)
+        time.sleep(2 * time_to_sleep * random.random())
+    logging.info("Done.")
+    logging.info("Please check if there is any error in `volunteer_log.txt`.")
+    input("Press `Enter` to exit.")
+
+if __name__ == '__main__':
+    main()
